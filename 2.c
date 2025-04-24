5c9e8760073968700a4c0d0171faa90b427b7744
whatislove <40031147+de-tout-mon-coeur@users.noreply.github.com>
2025-04-24 17:57:56 +0700

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void decToBin(int number)
+{
+    int bits = sizeof(number) * 8;
+    int i = bits - 1;
+    
+    for (i; i >= 0; i--)
+    {
+        int bit = (number >> i) & 1;
+        printf("%d", bit);
+    }
+}
+
+int main()
+{
+    int input;
+    
+    printf("Enter number: ");
+    scanf("%u", &input);
+    
+    printf("Binary: ");
+    decToBin(input);
+    
+    return 0;
+}
